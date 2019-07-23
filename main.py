a5155eaf147cb9c51b8cb6454c9f4327a3b35488
liyihan <liyihan@outlook.com>
2019-07-23 15:30:05 -0700

    remote training configuration

--- main.py
+++ main.py
@@ -1,55 +1,55 @@
 import os
 import sys
 
-print(sys.path)
-sys.path.append('/home/liyihan/anaconda3')
-
 lr = "0.01"
 batch_size = "30"
 arch = "resnet18"
 data_name = "ucf101"
 representation = "mv"
-data_root = "data/datalists/ucf101_split1_train.txt"
+data_root = "data/ucf101/mpeg4_videos"
 train_list = " data/datalists/ucf101_split1_train.txt"
 test_list = "data/datalists/ucf101_split1_test.txt"
 model_prefix = "ucf101_mv_model"
 lr_steps = "150 270 390"
 epochs = "510"
 gpus = "0 "
-# no_accumulation = "--no-accumulation"
+is_accumulated = True
 
-output_file = "training_"+data_name+"_"+representation+"_bs"+batch_size+"_accumulated"+".out"
+accu_option = ""
+if(is_accumulated):
+    accu_option = ""
+else:
+    accu_option = " --no_accumulation "
 
+if is_accumulated:
+    output_file = "training_"+data_name+"_"+representation+"_bs"+batch_size+"_accumulated"+".out"
+    # model_checkpoint = model_prefix+"_"+representation+"_checkpoint"+"_bs"+batch_size+"_accumulated"+".pth.tar"
+    # model_best = model_prefix+"_"+representation+"_best"+"_bs"+batch_size+"_accumulated"+".pth.tar"
+else:
+    output_file = "training_" + data_name + "_" + representation + "_bs" + batch_size + "_original" + ".out"
+    # model_checkpoint = model_prefix + "_" + representation + "_checkpoint" + "_bs" + batch_size + "_original" + ".pth.tar"
+    # model_best = model_prefix + "_" + representation + "_best" + "_bs" + batch_size + "_original" + ".pth.tar"
 
+# print(os.getcwd())
+# print(sys.executable)
 
-# os.system("nohup sh -c '" +
-#           sys.executable + " train.py --lr 0.01 >res1.txt && " +
-#           sys.executable + " train.py --lr 0.03 >res2.txt && " +
-#           sys.executable + " train.py --lr 0.09 >res3.txt" +
-#           "' &")
-# print("Enable anaconda")
-
-# if(os.system("conda activate coviar")==0):
-    # print(os.getcwd())
 print("Create output file")
 if os.path.exists(output_file):
-    print("Exists.")
+    print("File exists")
 else:
     os.system("touch "+ output_file)
     if os.access(output_file, os.W_OK):
-        print("Start executing")
-        os.system("conda activate coviar")
-        print(os.system("nohup sh -c '" +
+        print("Enable coviar")
+        os.system("/home/liyihan/anaconda3/bin/activate coviar")
+        print("Start training")
+        os.system("nohup sh -c '" +
                 sys.executable + " train.py --lr " + lr + " --batch-size " + batch_size + " --arch " + arch +
                 " --data-name " + data_name + " --representation " + representation +
                 " --data-root " + data_root + " --train-list " + train_list + " --test-list " + test_list +
                 " --model-prefix " + model_prefix + " --lr-steps " + lr_steps + " --epochs " + epochs +
-                " --gpus " + gpus +
+                " --gpus " + gpus + accu_option +
                 " > " + output_file +
-                "' &"))
-
-print("End")
-
+                "' &")
 
 
 
